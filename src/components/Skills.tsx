ae549dedde8aa1faf464f88a4e9df61d25c79b72
Tarani Yaragani <147273506+Tarani12@users.noreply.github.com>
2025-06-21 18:25:28 +0530

    Update Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -9,7 +9,7 @@ const Skills = () => {
       skills: [
         { name: "C", level: 85 },
         { name: "C++", level: 90 },
-        { name: "Java", level: 80 },
+        { name: "Java", level: 75 },
         { name: "Python", level: 90 }
       ]
     },
